86771062ddc184d3f0e4d751a17756152db648e4
Henrique Meneses <167329118+henrique1178@users.noreply.github.com>
2025-02-11 10:43:37 -0300

    Tipo de variavel em C

---
+++ tipodevariavel.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int idade;
+    float altura;
+    double peso;
+    char sexo;
+    char nome[50] = "Henrique";
+
+    idade = 18;
+    altura = 1.70;
+    peso = 63.5;
+    sexo = 'M';
+
+    return 0;
+}
